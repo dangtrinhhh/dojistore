7206d969528ffa865e1a2a5bb05399ccf34c5e0f
Đỗ Đăng Trình <82462470+dangtrinhhh@users.noreply.github.com>
2024-01-05 08:50:26 +0700

    refactor

--- dojistore/settings.py
+++ dojistore/settings.py
@@ -97,27 +97,27 @@ WSGI_APPLICATION = 'dojistore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'doubletbad',
-        'USER': 'doubletbad',
-        'PASSWORD': '@Double2T.',
-        'HOST': 'db',
-        'PORT': '5432',
-    }
-}
-
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'dojistore',
-#         'USER': 'postgres',
-#         'PASSWORD': '@Trinhdilam9.',
-#         'HOST': 'localhost', # db
+#         'NAME': 'doubletbad',
+#         'USER': 'doubletbad',
+#         'PASSWORD': '@Double2T.',
+#         'HOST': 'db',
 #         'PORT': '5432',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dojistore',
+        'USER': 'postgres',
+        'PASSWORD': '@Trinhdilam9.',
+        'HOST': 'localhost', # db
+        'PORT': '5432',
+    }
+}
 
 # DATABASES = {
 #   'default': {
